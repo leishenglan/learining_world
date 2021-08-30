216a40237596b2d6c1a11328989eb296a3dadde6
e <2676611490@qq.com>
2021-08-30 16:34:23 +0800

    Initial commit

---
+++ src/test/java/com/lsl/learining_world/LeariningWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsl.learining_world;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeariningWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
